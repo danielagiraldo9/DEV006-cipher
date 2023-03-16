2e7d2686d40c49f6030d1ea41f7f5b814f4cf723
danielagiraldo9 <danielagiraldoj@gmail.com>
2023-03-16 14:12:05 -0500

    Cambios en el HTML

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
 
-//console.log(cipher.encode(70, "DANIELA"));
+console.log(cipher.encode(70, "DANIELA"));
+
+const userName = document.getElementById('userName');
+const btn1 = document.getElementById('btn1');
+const saludo = document.getElementById('saludo');
+
+function captureName() {
+    saludo.innerText = 'Hola ' + userName.value + ' que quieres hacer hoy?'
+    // const name = document.createElement("h3");
+    // const text_name = document.createTextNode('Hola ' + userName.value + ' que quieres hacer hoy?');
+    // name.appendChild(text_name);
+    // document.body.appendChild(name);
+}
 
 
 
+function encodeButton() {
 
+}
+
+function decodeButton() {
+
+}
+
+btn1.addEventListener('click', captureName);
