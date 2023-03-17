57ec8210320176055e7c6049cc9ee50a674c0c0d
danielagiraldo9 <danielagiraldoj@gmail.com>
2023-03-16 23:47:08 -0500

    Arrego errores de identación

--- src/cipher.js
+++ src/cipher.js
@@ -1,41 +1,40 @@
 const cipher = {
   encode (offset, string) {
-    let spacesToRun = offset % 26;
+    const spacesToRun = offset % 26;
     let encodedMessage = "";
     for (let i = 0; i < string.length; i++) {
-        if (string[i] == " ") {
-            encodedMessage += " ";
+      if (string[i] === " ") {
+        encodedMessage += " ";
+      } else {
+        const letterCipherOne = string.charCodeAt(i) + spacesToRun;
+        if (letterCipherOne > 90) {
+          const res = letterCipherOne - 90;
+          const letterCipherTwo = 64 + res;
+          encodedMessage += String.fromCharCode(letterCipherTwo);
         } else {
-            let letterCipherOne = string.charCodeAt(i) + spacesToRun;
-            if (letterCipherOne > 90) {
-                let res = letterCipherOne - 90;
-                let letterCipherTwo = 64 + res;
-                encodedMessage += String.fromCharCode(letterCipherTwo);
-            } else {
-                encodedMessage += String.fromCharCode(letterCipherOne);
-            }
+          encodedMessage += String.fromCharCode(letterCipherOne);
         }
+      }
     }
-
     return encodedMessage;
   },
 
   decode (offset, string) {
-    let spacesToRun = offset % 26;
+    const spacesToRun = offset % 26;
     let decodeMessaje = "";
     for (let i = 0; i < string.length; i++) {
-        if (string[i] == " ") {
-            decodeMessaje += " ";
+      if (string[i] === " ") {
+        decodeMessaje += " ";
+      } else {
+        const letterDecipherOne = string.charCodeAt(i) - spacesToRun;
+        if (letterDecipherOne < 65) {
+          const res =  letterDecipherOne - 65;
+          const letterDecipherTwo = 91 + res
+          decodeMessaje += String.fromCharCode(letterDecipherTwo);
         } else {
-           let letterDecipherOne = string.charCodeAt(i) - spacesToRun;
-           if (letterDecipherOne < 65) {
-              let res =  letterDecipherOne - 65;
-              let letterDecipherTwo = 91 + res
-              decodeMessaje += String.fromCharCode(letterDecipherTwo);
-           } else {
-              decodeMessaje += String.fromCharCode(letterDecipherOne);
-           }
+          decodeMessaje += String.fromCharCode(letterDecipherOne);
         }
+      }
     }
     return decodeMessaje;
   }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 //import cipher from './cipher.js';
 
-console.log(cipher.encode(70, "DANIELA"));
+//console.log(cipher.encode(70, "DANIELA HOLA"));
 
 const userName = document.getElementById('userName');
 const btn1 = document.getElementById('btn1');
 const saludo = document.getElementById('saludo');
 
 function captureName() {
-    saludo.innerText = 'Hola ' + userName.value + ' que quieres hacer hoy?'
-    // const name = document.createElement("h3");
-    // const text_name = document.createTextNode('Hola ' + userName.value + ' que quieres hacer hoy?');
-    // name.appendChild(text_name);
-    // document.body.appendChild(name);
+  saludo.innerText = 'Hola ' + userName.value + ' que quieres hacer hoy?';
+  // const name = document.createElement("h3");
+  // const text_name = document.createTextNode('Hola ' + userName.value + ' que quieres hacer hoy?');
+  // name.appendChild(text_name);
+  // document.body.appendChild(name);
 }
 
 
 
-function encodeButton() {
+// function encodeButton() {
 
-}
+// }
 
-function decodeButton() {
+// function decodeButton() {
 
-}
+// }
 
 btn1.addEventListener('click', captureName);
